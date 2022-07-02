a3e2e0551635a7439bab6ef40b146978a34df405
MadhushaPrasad <madushaprasad21@gmail.com>
2022-07-02 12:51:40 +0530

    test how selecter is working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
