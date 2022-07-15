1ceffa30a67a2e60333171c58d932556b96593c3
MadhushaPrasad <madushaprasad21@gmail.com>
2022-07-15 23:29:47 +0530

    authGuard

--- src/app/guard/auth-guard.service.ts
+++ src/app/guard/auth-guard.service.ts
@@ -1,18 +1,9 @@
-import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from "@angular/router";
-import {Observable} from "rxjs";
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard {
 
-  constructor() {
-  }
-
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
-    Observable<boolean | UrlTree> | Promise<boolean |
-    UrlTree> | boolean | UrlTree {
-    return false;
-  }
+  constructor() { }
 }
